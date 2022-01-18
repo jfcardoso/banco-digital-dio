e3e301b7c1f4e077636bf7d0246027089b481b0a
jfcardoso <maestro.jfc@gmail.com>
2022-01-18 18:02:54 -0300

    first commit

---
+++ src/AppBanco.java
@@ -0,0 +1,17 @@
+public class AppBanco {
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Paola");
+
+        Conta cc = new ContaCorrente(cliente);
+        Conta poupanca = new ContaPoupanca(cliente);
+
+        cc.depositar(120);
+        cc.transferir(80, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
+}
